d8f04a1f200b6902c4df688e9ab57a3c69e730b1
Tomas Psota <to.psota@gmail.com>
2024-01-03 17:08:52 +0100

    feat(ts): rename callbacks

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,6 +1,11 @@
+import { Capacitor } from "@capacitor/core";
+
 export interface FreeraspPlugin {
-  addListener(listener: string, callback: () => any): any;
+  addListener(listener: string, callback: (event: any) => void): any;
   talsecStart(options: { config: FreeraspConfig }): Promise<{ started: boolean }>;
+  onInvalidCallback(): void;
+  getThreatIdentifiers(): Promise<{ ids: number[] }>;
+  getThreatChannelData(): Promise<{ ids: [string, string] }>;
 }
 
 export type FreeraspConfig = {
@@ -34,3 +39,53 @@ export type NativeEventEmitterActions = {
   secureHardwareNotAvailable?: () => any;
   obfuscationIssues?: () => any;
 };
+
+export class Threat {
+  value: number;
+
+  static AppIntegrity = new Threat(0);
+  static PrivilegedAccess = new Threat(0);
+  static Debug = new Threat(0);
+  static Hooks = new Threat(0);
+  static Passcode = new Threat(0);
+  static Simulator = new Threat(0);
+  static SecureHardwareNotAvailable = new Threat(0);
+  static DeviceBinding = new Threat(0);
+  static DeviceID = new Threat(0);
+  static UnofficialStore = new Threat(0);
+  static Overlay = new Threat(0);
+  static ObfuscationIssues = new Threat(0);
+
+  constructor(value: number) {
+    this.value = value;
+  }
+
+  static getValues(): Threat[] {
+    return Capacitor.getPlatform() === 'android'
+      ? [
+          this.AppIntegrity,
+          this.PrivilegedAccess,
+          this.Debug,
+          this.Hooks,
+          this.Passcode,
+          this.Simulator,
+          this.SecureHardwareNotAvailable,
+          this.DeviceBinding,
+          this.UnofficialStore,
+          this.Overlay,
+          this.ObfuscationIssues,
+        ]
+      : [
+          this.AppIntegrity,
+          this.PrivilegedAccess,
+          this.Debug,
+          this.Hooks,
+          this.Passcode,
+          this.Simulator,
+          this.SecureHardwareNotAvailable,
+          this.DeviceBinding,
+          this.DeviceID,
+          this.UnofficialStore,
+        ];
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
-import { Capacitor, registerPlugin } from '@capacitor/core';
+import { registerPlugin } from '@capacitor/core';
 
 import type { FreeraspPlugin, FreeraspConfig, NativeEventEmitterActions } from './definitions';
+import { Threat } from './definitions';
+import { getThreatCount, itemsHaveType } from './utils';
 
 const activeListeners: any[] = [];
 
@@ -8,17 +10,81 @@ const Freerasp = registerPlugin<FreeraspPlugin>('Freerasp', {
   web: () => import('./web').then(m => new m.FreeraspWeb()),
 });
 
+const onInvalidCallback = (): void => {
+  Freerasp.onInvalidCallback();
+};
+
+const getThreatIdentifiers = async (): Promise<number[]> => {
+  const { ids } = await Freerasp.getThreatIdentifiers();
+  if (ids.length !== getThreatCount() || !itemsHaveType(ids, 'number')) {
+    onInvalidCallback();
+  }
+  return ids;
+};
+
+const getThreatChannelData = async (): Promise<[string, string]> => {
+  const { ids } = await Freerasp.getThreatChannelData();
+  if (ids.length !== 2 || !itemsHaveType(ids, 'string')) {
+    onInvalidCallback();
+  }
+  return ids;
+};
+
+const prepareMapping = async (): Promise<void> => {
+  const newValues = await getThreatIdentifiers();
+  const threats = Threat.getValues();
+
+  threats.map((threat, index) => {
+    threat.value = newValues[index]!;
+  });
+};
+
 const setThreatListeners = async <T extends NativeEventEmitterActions>(
   callbacks: T & Record<Exclude<keyof T, keyof NativeEventEmitterActions>, []>
 ) => {
+    const [channel, key] = await getThreatChannelData();
+    await prepareMapping();
 
-  for (const [threat, action] of Object.entries(callbacks)) {
-    if ((threat === 'obfuscationIssues' && Capacitor.getPlatform() === 'ios') || (action === undefined)) {
-      continue;
+    await Freerasp.addListener(channel, (event: any) => {
+    if (event[key] === undefined) {
+      onInvalidCallback();
     }
-    const listener = await Freerasp.addListener(threat, action);
-    activeListeners.push(listener);
-  }
+    switch (event[key]) {
+      case Threat.PrivilegedAccess.value:
+        callbacks.privilegedAccess?.();
+        break;
+      case Threat.Debug.value:
+        callbacks.debug?.();
+        break;
+      case Threat.Simulator.value:
+        callbacks.simulator?.();
+        break;
+      case Threat.AppIntegrity.value:
+        callbacks.appIntegrity?.();
+        break;
+      case Threat.UnofficialStore.value:
+        callbacks.unofficialStore?.();
+        break;
+      case Threat.Hooks.value:
+        callbacks.hooks?.();
+        break;
+      case Threat.DeviceBinding.value:
+        callbacks.deviceBinding?.();
+        break;
+      case Threat.Passcode.value:
+        callbacks.passcode?.();
+        break;
+      case Threat.SecureHardwareNotAvailable.value:
+        callbacks.secureHardwareNotAvailable?.();
+        break;
+      case Threat.ObfuscationIssues.value:
+        callbacks.obfuscationIssues?.();
+        break;
+      default:
+        onInvalidCallback();
+        break;
+    }
+  });
 };
 
 const removeThreatListeners = () => {
